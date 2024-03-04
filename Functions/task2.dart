57029410e289949da63db05de31e883da9a4eb82
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-04 10:08:21 +0300

    task 2

---
+++ Functions/task2.dart
@@ -0,0 +1,11 @@
+// addTwo Function that adds two numbers and outputs the sum
+void substractTwo(int w, int z) {
+  //parameters x and y
+  int sub = w - z;
+  print("The difference of x and y is: $sub");
+
+}
+void main() {
+  substractTwo(72, 27);
+  //call the function with two values
+}
