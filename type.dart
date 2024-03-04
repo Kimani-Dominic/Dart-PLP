4c4e52618f356b73761d9ee29fdc93c81b58c5a4
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-04 08:38:38 +0300

    Data types

---
+++ type.dart
@@ -0,0 +1,35 @@
+//Various Data types in Dart
+
+void main() {
+  // Integer data type
+  int age = 25;
+
+  // Double data type
+  double height = 5.9;
+
+  // String data type
+  String name = "Kimani";
+  String school = "PLP Academy";
+
+  // Boolean data type
+  bool isStudent = true;
+
+  // List data type
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Map data type
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'isStudent': false,
+  };
+
+  // Printing values
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+  print("Numbers: $numbers");
+  print("Person: $person");
+  print("I'm $name a student at $school");
+}
