9ce19fc944b67e0c3ef4ee97ceb2233b93f2c5a3
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-04 09:53:30 +0300

    task 1

---
+++ Functions/task1.dart
@@ -0,0 +1,11 @@
+// addTwo Function that adds two numbers and outputs the sum
+void addTwo(int x, int y) {
+  //parameters x and y
+  int sum = x + y;
+  print("The sum of x and y is: $sum");
+
+}
+void main() {
+  addTwo(36, 89);
+  //call the function with two values
+}
