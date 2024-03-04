df39d5ece2b99776b72ff23aa25825fefc4b2c65
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-04 15:32:24 +0300

    stringLength

---
+++ Functions/task5.dart
@@ -0,0 +1,12 @@
+//checking the length of strings
+
+
+void stringLength(String input) {
+  int length= input.length;
+  print("$input has $length letters");
+}
+void main() {
+  stringLength('Kenyan');
+}
+
+
