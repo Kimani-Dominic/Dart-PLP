565205b6da372294c918d6d5ea0b9d3c51847fe6
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-07 22:25:50 +0300

    Control flow

---
+++ control_flow/soln.dart
@@ -0,0 +1,16 @@
+//If the number is greater than 10, print "Your number is greater than 10"
+//If the number is less than 10, print "Your number is less than 10"
+//If the number is equal to 10, print "Your number is equal to 10"
+
+void main() {
+  int number = 1;
+  if (number > 10){
+    print("Your number is greater than 10");
+  }
+  else if(number == 10 ){
+  print("Your number is equal to 10");
+  }
+  else{
+    print("Your number is less than 10");
+  }
+}
