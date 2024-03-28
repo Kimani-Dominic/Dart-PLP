2625e062a019c149ec5b6fd4e6ddb85b97ea41b1
Kimani-Dominic <muiruridominic001@gmail.com>
2024-03-28 19:27:11 +0300

    Date and Time

--- utilities/time.dart
+++ utilities/time.dart
@@ -6,14 +6,35 @@ DateTime futureDATE = now.add(Duration(days: 7));
 //previous date
 DateTime previousDate = now.subtract(Duration(days: 28));
 
+DateTime earlierDate = DateTime(2024, 3, 20);
+
+  // Parse a string to DateTime
+DateTime parsedDate = DateTime.parse('2024-04-05');
 
 
+// Function to get month abbreviation
+String _getMonthAbbreviation(int month) {
+  return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][month - 1];
+}
+ 
 
-
+  // Format the parsed date in a custom format
+String customFormattedDate = "${_getMonthAbbreviation(parsedDate.month)} ${parsedDate.day}, ${parsedDate.year}";
 
 
 
   print("Current date and time is: $now");
   print("Current date and time is: $futureDATE");
   print("Previous earlier dat: $previousDate");
+  print("an earlier date: $earlierDate");
+  print("Custom Formatted Date: $customFormattedDate");
+  print(customFormattedDate);
+
+// Print specific components of the current date
+  print("Year: ${now.year}");
+  print("Month: ${now.month}");
+  print("Day: ${now.day}");
+  print("Hour: ${now.hour}");
+  print("Minute: ${now.minute}");
+  print("Second: ${now.second}");
 }
