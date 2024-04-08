6efc47f933390a29bfc9b64a6b6eed871caae5d0
Kimani-Dominic <muiruridominic001@gmail.com>
2024-04-08 22:56:21 +0300

    Loop

---
+++ Utilities_Assign/task2.dart
@@ -0,0 +1,8 @@
+//Write a program that uses a for loop to print out the numbers from 1 to 10.
+
+void main() {
+  print("Printing numbers from 1 to 10:");
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+  }
+}
