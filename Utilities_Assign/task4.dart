92a51cd4e4a3509bf2a4a113ae0e0194844e8b8c
Kimani-Dominic <muiruridominic001@gmail.com>
2024-04-09 22:34:17 +0300

    While Loop

---
+++ Utilities_Assign/task4.dart
@@ -0,0 +1,12 @@
+//Create a program that uses a while loop to print out the numbers from 20 to 10.
+
+void main() {
+  int i = 20; // Start value
+
+  print("Printing numbers from 20 to 10:");
+
+  while (i >= 10) {
+    print(i);
+    i--; // Decrement i by 1 in each iteration
+  }
+}
