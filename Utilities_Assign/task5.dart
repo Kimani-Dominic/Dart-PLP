60287cdaf60211468cc953fa4eddd42bfe611f4c
Kimani-Dominic <muiruridominic001@gmail.com>
2024-04-09 22:46:07 +0300

    if-else statement

---
+++ Utilities_Assign/task5.dart
@@ -0,0 +1,11 @@
+//Create a program that uses an if-else statement to check if a number is even or odd and output the result.
+
+void main() {
+int number = 5;
+if (number % 2 == 0){
+  print("$number is even");
+}
+else{
+  print("$number is odd");
+}
+}
